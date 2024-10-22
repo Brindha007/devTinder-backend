2273d5cb8391f0012597ef26b8f7e6d0bceb7e11
Brindha007 <d.brindha25@rediffmail.com>
2024-10-22 20:03:16 +0530

    Exploring get,post,delete in Postman

--- src/app.js
+++ src/app.js
@@ -2,6 +2,25 @@ const express = require("express");
 
 const app = express();
 
+app.get("/user",(req, res) => {
+    res.send({firstName : "Brindha", lastName : "D"})
+});
+
+app.post("/user",(req, res) => {
+    res.send("Data saved in DB");
+});
+
+app.delete("/user", (req, res) => {
+    res.send("Data deleted in DB");
+});
+
+app.patch("/user",(req,res) => {
+    res.send("Updated partial data.")
+});
+/* app.use("/test/win",(req, res) => {
+    res.send("The Universe wants me to win!!!");
+    console.log("test win url loaded");
+});
 app.use("/test",(req, res) => {
     res.send("We win!!!");
     console.log("test url loaded");
@@ -11,7 +30,7 @@ app.use("/test",(req, res) => {
 app.use("/",(req, res) => {
     res.send("Hello Universe!!!");
     console.log("loaded");
-});
+}); */
 
 
 app.listen(3000, () => {
